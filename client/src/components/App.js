e25c4f92a67ea5395f2316dfd1e35b25c7271ac4
jinho6225 <jinho6225@hotmail.com>
2020-02-17 00:28:57 -0800

    render img data from App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Collection from './Collection'
 import ImageDisplay from './ImageDisplay'
 import ImageFetcher from './ImageFetcher'
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,11 +11,23 @@ class App extends React.Component {
       collections: []
     }
     this.addImg = this.addImg.bind(this)
+    this.getImg = this.getImg.bind(this)
   }
 
   componentDidMount () {
     const { collections } = this.props;
     this.setState({ collections });
+  }
+
+  getImg() {
+    const addImg = this.addImg
+    axios.get(`https://source.unsplash.com/random/100x100`)
+      .then(function (response) {
+        addImg(response.request.responseURL);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }
 
   addImg(imgURL) {
@@ -27,7 +40,7 @@ class App extends React.Component {
     const { collections } = this.state;
     return (
       <div id="mainApp">
-        <ImageFetcher addImg={this.addImg} />
+        <ImageFetcher getImg={this.getImg} />
         <div className="collectionRow">
           <ImageDisplay />
           <Collection collections={collections}/>
--- client/src/components/ImageFetcher.js
+++ client/src/components/ImageFetcher.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import axios from 'axios';
 
 class ImageFetcher extends React.Component {
   constructor(props) {
     super(props)
-    this.getImg = this.getImg.bind(this)
-  }
-
-  getImg() {
-    const { addImg } = this.props
-    axios.get(`https://source.unsplash.com/random/100x100`)
-      .then(function (response) {
-        addImg(response.request.responseURL);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
   }
 
   render() {
+    const { getImg } = this.props
     return (
       <div className="container">
-          <button onClick={this.getImg} >Add Picture</button>
+          <button onClick={getImg} >Add Picture</button>
       </div>
     )
   }
